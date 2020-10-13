f91c54e39d98a43ab8aa31d1204ce69932fd4236
srj <srj@iMac-SRJ.local>
2020-10-13 09:17:14 +0300

    add data time

---
+++ js/scripts.js
@@ -0,0 +1,52 @@
+function renderTime(){
+	// date
+	var mydate = new Date();
+	var year = mydate.getYear();
+		if (year < 1000){
+			year += 1900
+		}
+	var day = mydate.getDay();
+	var month = mydate.getMonth();
+	var daym = mydate.getDate();
+	var dayarray = new Array ("Sunday","Monday","Tuesday","Wednesday","Thurstday","Friday","Saturday");
+	var montharray = new Array ("Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec");
+	// Date end
+	
+	// Time
+	var currentTime= new Date();
+	var h = currentTime.getHours();
+	var m = currentTime.getMinutes();
+	var s = currentTime.getSeconds();
+		if (h == 24){
+			h = 0;
+		}
+		
+		else if(h > 12){
+			h = h - 0;
+		}
+
+		if (h < 10) {
+			h = "0" + h;
+		}
+
+		if(m<10){
+			m = "0"+ m;
+		}
+
+		if (s<10){
+			s = "0" + s;
+		}
+
+		var myClock = document.getElementById("clockDisplay");
+		myClock.textContent = "" + dayarray[day] + " " + daym + " " + montharray[month] + " " + year + " | " + h + ":" + m + ":" + s;
+		myClock.innerText = "" + dayarray[day] + " " + daym + " " + montharray[month] + " " + year + " | " + h + ":" + m + ":" + s;
+
+		setTimeout("renderTime()",1000);
+		
+}
+renderTime();
+
+
+// Just put this code and it works
+// myclock.innerText = dayarray[day] + " " + daym+  " " + montharray[month] + " " + year + " | " + h + " : " + m + " : " + s;
+// Thanks to Philosofisk
